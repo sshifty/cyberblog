9c45b775ca5ba00d74f182a46776436e404d45a3
Alex <atiszon@gmail.com>
2022-02-25 14:47:46 +0100

    deploy to heroku

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const publicPath = path.join(__dirname, '..', 'public');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -30,7 +31,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname,"./client/build")));
+
 app.use(compression());
 app.use(helmet());
 
